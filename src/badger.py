0b67bee21180f38480220c240ca2076e6df53af7
Jacob Marks <jamarks13@gmail.com>
2023-08-25 20:44:41 -0700

    upgrading go-wild!

--- src/badger.py
+++ src/badger.py
@@ -8,6 +8,7 @@
 import argparse
 import base64
 import os
+import re
 import pyperclip
 import yaml
 from xml.dom import minidom
@@ -22,6 +23,14 @@ visually appealing. The SVG should be suitable for use as a badge logo. Include
 some details in the design, so that it stands out from other badges. Only return
 the SVG data and XML header, not any explanatory text. The badge should NOT have
 any text in it, as that will be added later.
+"""
+
+FIX_BADGE_PROMPT = """
+Turn the text below into a working SVG code for SUBJECT. The SVG code should be 
+properly formatted and include the XML header. The badge should NOT have any 
+text in it, as that will be added later. Just return the SVG data and XML 
+header, not any explanatory text. You will be penalized if the SVG code is not
+properly formatted, or if it contains any text. The text to correct is:\n\n
 """
 
 # Function to create a default config file with a 'badger' badge
@@ -111,20 +120,6 @@ def list_badges(badges):
         )
 
 
-# def change_svg_color(svg_data, new_color="#000000"):
-#     # Parse the SVG data
-#     doc = minidom.parseString(svg_data)
-
-#     # Find all 'fill' attributes
-#     for element_name in ["path", "circle", "rect", "g"]:
-#         for element in doc.getElementsByTagName(element_name):
-#             if element.hasAttribute("fill"):
-#                 element.setAttribute("fill", new_color)
-
-#     # Convert the SVG back to a string
-#     return doc.toxml().encode("utf-8")
-
-
 def change_svg_color(svg_data, new_color="#000000"):
     doc = minidom.parseString(svg_data)
 
@@ -204,7 +199,7 @@ def generate_random_svg(subject):
     openai.api_key = os.getenv("OPENAI_API_KEY")
     print("Calling the OpenAI API... (this may take a few seconds)")
     completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-4",
         messages=[
             {
                 "role": "user",
@@ -213,16 +208,36 @@ def generate_random_svg(subject):
         ],
     )
     response = completion.choices[0].message["content"]
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            {
+                "role": "user",
+                "content": FIX_BADGE_PROMPT.replace("SUBJECT", subject)
+                + response,
+            },
+        ],
+    )
+    response = completion.choices[0].message["content"]
+
     svg = response.replace('\\"', '"').replace("\\n", "\n").strip()
-    return svg.encode("utf-8")
+    svg = re.search(r"<svg.*?</svg>", svg, re.DOTALL)
+    if svg:
+        return svg.group(0).encode("utf-8")
+    else:
+        print("GPT-3.5 failed to generate a valid SVG. Please try again.")
+        return
 
 
-def generate_trial_badge(svg_data):
+def generate_trial_badge(svg_data, subject):
     # Encode in base64 and decode it to ASCII
     b64_logo = base64.b64encode(svg_data).decode("ascii")
 
+    subject = subject.replace(" ", "%20").strip()
+
     badge_pattern = (
-        f"[![Trial Badge](https://img.shields.io/badge/text-blue.svg?"
+        f"[![Trial Badge](https://img.shields.io/badge/{subject}-blue.svg?"
         f"style=flat&logo=data:image/svg+xml;base64,{b64_logo})](https://github.com/voxel51/badger)"
     )
     return badge_pattern
@@ -401,8 +416,10 @@ def main():
             return
 
         svg_data = generate_random_svg(args.prompt)
+        if not svg_data:
+            return
         # Copy to clipboard
-        pyperclip.copy(generate_trial_badge(svg_data))
+        pyperclip.copy(generate_trial_badge(svg_data, args.prompt))
 
         # pyperclip.copy(svg_data)
 
